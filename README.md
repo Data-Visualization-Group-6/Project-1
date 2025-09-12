e67722c1f2656f68e2e834a1b9d35f8eb210b30a
tensofu <148308017+TerriblyFriedRice@users.noreply.github.com>
2025-09-11 23:40:00 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Project-1
-Class project for visualizing a dataset
+# Project
+World Happiness Dataset
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import ParallelCoordinates from './components/ParallelCoordinates/ParallelCoordinates'
+
+function App() {
+  return (
+    <>
+      <ParallelCoordinates />
+    </>
+  )
+}
+
+export default App
